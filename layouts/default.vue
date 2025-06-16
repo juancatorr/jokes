f00b020f64d966f61a6cf892c30097b7aa559f1d
juancatorr <clavijojc@gmail.com>
2025-06-16 19:51:03 -0300

    chore: add Biome linting and formatting with Husky pre-commit hooks

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script setup>
-import Header from '~/components/Header.vue'
-import Footer from '~/components/Footer.vue'
+import Footer from '~/components/Footer.vue';
+import Header from '~/components/Header.vue';
 </script>
 
 <style scoped>
