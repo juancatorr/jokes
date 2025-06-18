6287d7af8d01c9f027e55790b8bafaf40bcc54f8
juancatorr <clavijojc@gmail.com>
2025-06-18 12:37:20 -0300

    chore: switch API base URL to local development server

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { defineNuxtPlugin } from '#app';
 
 const api = axios.create({
-  baseURL: 'https://official-joke-api.appspot.com/',
+  // baseURL: 'https://official-joke-api.appspot.com/',
+  baseURL: 'http://localhost:3005/',
 });
 
 export default defineNuxtPlugin((nuxtApp) => {
